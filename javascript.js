f986ebe8f2551bed789209f27988628ac81f7024
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-30 19:15:43 +1000

    Fixed variable being reassigned rather then boolean checked. Whoops!

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ let operator;
 let displayValue = "";
 let currentOperator;
 let textTest;
-let result = undefined;
+let result;
 const plus = '+';
 const minus = '-';
 const times = '*';
@@ -26,17 +26,23 @@ let i = 0;
 // until you hit clear button (Which I haven't made yet. heh.)
 
 addition = () => {
-    return parseFloat(numberOne) + parseFloat(numberTwo);
+    result = parseFloat(numberOne) + parseFloat(numberTwo);
+    displayValue = result;
+    return result;
 };
 
 
 subtraction = () => {
-    return numberOne - numberTwo;
+    result = numberOne-numberTwo;
+    displayValue = result;
+    return result;
 };
 
 
 multiplication = () => {
-    return numberOne * numberTwo;
+    result = numberOne * numberTwo;
+    displayValue = result;
+    return result;
 };
 
 
@@ -48,9 +54,18 @@ division = () => {
 
 
 operate = () => {
-    if (operator = dividedBy) {
+    if (operator == plus) {
+        return addition();
+    };
+    if (operator == minus) {
+        return subtraction();
+    };
+    if (operator == times) {
+        return multiplication();
+    };
+    if (operator == dividedBy) {
         return division();
-    }
+    };
 };
 
 
@@ -59,118 +74,143 @@ for (let i = 0; i < buttons.length; i++) {
         currentOperator = i;
         displayValue += buttons[i].innerHTML;
         console.log("You have pressed " + i);
+        
         switch (currentOperator) {
             case 0:
                 console.log("You pressed the right one, WELL DONE!");
                 if  (operator == undefined) {
                     numberOne += "7";
                     break;
-                }
+                };
                 numberTwo += "7";
                 break;
+
             case 1:
                 console.log("This is the number 8 key");
                 if (operator == undefined) {
                     numberOne += "8";
                     break;
-                }
+                };
                 numberTwo += "8";
                 break;
+
             case 2:
                 console.log("This is the number 9 key");
                 if (operator == undefined) {
                     numberOne += "9";
                     break;
-                }
+                };
                 numberTwo += "9";
                 break;
+
             case 3:
                 operator = dividedBy;
                 break;
+
             case 4:
                 console.log("This is the number 4 key");
                 if (operator == undefined) {
                     numberOne += "4";
                     break;
-                }
+                };
                 numberTwo += "4";
                 break;
+
             case 5:
                 console.log("This is the number 5 key");
                 if (operator == undefined) {
                     numberOne += "5";
                     break;
-                }
+                };
                 numberTwo += "5";
                 break;
+
             case 6:
                 console.log("This is the number 6 key");
                 if (operator == undefined) {
                     numberOne += "6";
                     break;
-                }
+                };
                 numberTwo += "6";
                 break;
+                
             case 7:
-                operator = dividedBy;
+                operator = times;
                 break;
+
             case 8:
                 console.log("This is the 1 key");
                 if (operator == undefined) {
                     numberOne += "1";
                     break;
-                }
+                };
                 numberOne += "1";
                 break;
+
             case 9:
                 console.log("this is the number 2 key");
                 if (operator == undefined) {
                     numberOne += "2";
                     break;
-                }
+                };
                 numberTwo += "2";
                 break;
+
             case 10:
                 console.log("This is the number 3 key");
                 if (operator == undefined) {
                     numberOne += "3";
                     break;
-                }
-                numberTwo += "2";
+                };
+                numberTwo += "3";
                 break;
+
             case 11: 
                 console.log("This is the subtraction key");
-                operator == minus;
+                operator = minus;
                 break;
+
             case 12:
                 console.log("This is the number 0 key");
                 if (operator == undefined) {
                     numberOne += "0";
                     break;
-                }
+                };
                 numberTwo += "0";
                 break;
+
             case 13:
                 console.log("This is the decimal key");
                 if (operator == undefined) {
                     numberOne += ".";
                     break;
-                }
-                numberTwo = "."
+                };
+                numberTwo = ".";
                 break;
+
             case 14:
                 console.log(operate());
-                return operate();
+                operate();
+                if (result !== undefined) {
+                    numberOne = result;
+                    result = undefined;
+                    numberTwo = "";
+                    operator = undefined;
+                    break;
+                };
+
             case 15:
                 console.log("This is the addition key");
                 operator = plus;
                 break;
+
             default:
                 break;
         }
         console.log(displayValue);
     });
 };
+
 
 // buttons[3].addEventListener("click", () => {
 //     console.log("The pressed the division key");
