f16e84f40ec0b2abeccd6946df8fef5eeef079d1
Brendan <brendanmcgaw@gmail.com>
2023-09-30 16:56:40 +1000

    Finished switch statement

--- javascript.js
+++ javascript.js
@@ -41,8 +41,9 @@ multiplication = () => {
 
 
 division = () => {
-    return numberOne / numberTwo;
-
+    result = numberOne / numberTwo;
+    displayValue = result;
+    return result;
 };
 
 
@@ -89,7 +90,7 @@ for (let i = 0; i < buttons.length; i++) {
                 numberTwo += "9";
                 break;
             case 3:
-                operator = "/"
+                operator = dividedBy;
                 break;
             case 4:
                 console.log("This is the number 4 key");
@@ -109,12 +110,70 @@ for (let i = 0; i < buttons.length; i++) {
                 break;
             case 6:
                 console.log("This is the number 6 key");
+                if (operator == undefined) {
+                    numberOne += "6";
+                    break;
+                }
+                numberTwo += "6";
+                break;
+            case 7:
+                operator = dividedBy;
+                break;
+            case 8:
+                console.log("This is the 1 key");
+                if (operator == undefined) {
+                    numberOne += "1";
+                    break;
+                }
+                numberOne += "1";
+                break;
+            case 9:
+                console.log("this is the number 2 key");
+                if (operator == undefined) {
+                    numberOne += "2";
+                    break;
+                }
+                numberTwo += "2";
+                break;
+            case 10:
+                console.log("This is the number 3 key");
+                if (operator == undefined) {
+                    numberOne += "3";
+                    break;
+                }
+                numberTwo += "2";
+                break;
+            case 11: 
+                console.log("This is the subtraction key");
+                operator == minus;
+                break;
+            case 12:
+                console.log("This is the number 0 key");
+                if (operator == undefined) {
+                    numberOne += "0";
+                    break;
+                }
+                numberTwo += "0";
+                break;
+            case 13:
+                console.log("This is the decimal key");
+                if (operator == undefined) {
+                    numberOne += ".";
+                    break;
+                }
+                numberTwo = "."
+                break;
             case 14:
                 console.log(operate());
-                result = operate();
+                return operate();
+            case 15:
+                console.log("This is the addition key");
+                operator = plus;
+                break;
             default:
                 break;
         }
+        console.log(displayValue);
     });
 };
 
